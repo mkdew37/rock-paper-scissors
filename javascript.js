a46377cc02f7e396d8e7ff9a14863c3e9f15c16d
mkdew37 <142149906+mkdew37@users.noreply.github.com>
2024-02-09 18:41:06 +0100

    Started working on the score feauture

--- javascript.js
+++ javascript.js
@@ -9,14 +9,13 @@ function getComputerChoice() {
         return pick[Math.floor(Math.random() * pick.length)];
 }
 
-// Prompts for the player to choose hand. Generates computers choice from function.
-
-
+let userScore = 0;
+let computerScore = 0;
 
 // This function plays a single round of Rock, Paper, Scissors
 function playRound()  {
         
-        const playerSelection = prompt("Rock, paper or scissor?");
+        const playerSelection = prompt("Choose your weapon: Rock, paper or scissor?");
         const computerSelection = getComputerChoice();
 
         if (playerSelection.toLowerCase() === computerSelection)    {
@@ -24,24 +23,31 @@ function playRound()  {
         }
         else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissor")    {
                 return "You win! Rock beats scissors.";
+                userScore++;
         }
         else if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper")   {
                 return "You loose! Paper beats rock.";
+                computerScore++;
         }
         else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock")   {
                 return "You win! Paper beats rock.";
+                userScore++;
         }
         else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissor")   {
                 return "You loose! Scissors beats paper.";
+                computerScore++;
         }
         else if (playerSelection.toLowerCase() === "scissor" && computerSelection === "paper")    {
                 return "You win! Scissors beats paper.";
+                userScore++;
         }
         else if (playerSelection.toLowerCase() === "scissor" && computerSelection === "rock")    {
                 return "You loose! Rock beats scissors.";
+                computerScore++;
         }
 }
 
+// Plays 5 rounds of Rock, paper, scissor!
 function playGame()     {
 
         console.log(playRound());
